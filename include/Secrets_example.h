eaca88eb8becea1424a5db534cf648a53be4d244
Igor Kovryzhkin <igorkovr@gmail.com>
2024-10-27 14:58:55 +0100

    Add README

---
+++ include/Secrets_example.h
@@ -0,0 +1,7 @@
+#define SECRET_SSID "SSID_1"
+#define SECRET_PASS "PASS_1"
+#define SECRET_SSID1 "SSID_2"
+#define SECRET_PASS1 "PASS_2"
+#define SECRET_SSID2 "SSID_3"
+#define SECRET_PASS2 "PASS_3"
+// ... and so on
